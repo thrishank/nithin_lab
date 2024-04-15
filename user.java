11a4c47a3000cd92e425a7f71f00a4e76f3960d7
mpvl07 <dommetimohan104@gmail.com>
2024-04-15 12:51:25 +0530

    user

---
+++ user.java
@@ -0,0 +1,36 @@
+package web_auto;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Mynta {
+
+	public static void main(String[] args)throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mohan\\Desktop\\SE\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		//WebDriver webDriver;
+        
+		driver.navigate().to("http://localhost:4000/");
+		//driver.wait(10);
+		driver.manage().window().maximize();
+		//driver.findElement(By.linkText("Login")).click();
+		//driver.findElement(By.linkText("Sign in")).click();
+		 //driver.wait(10);
+		driver.findElement(By.id("email")).click();
+		driver.findElement(By.id("email")).sendKeys("user@gmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).click();
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("111");
+		driver.findElement(By.id("submit-btn")).click();
+
+		
+        //driver.findElement(By.xpath("//div[@class='VfPpkd-RLmnJb']")).click();
+       //Thread.sleep(30);
+      
+        //driver.close();
+	}
+
+}
